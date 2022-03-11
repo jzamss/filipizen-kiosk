5798f914ab2327c8c94f45d3b9f66359f1bf34a5
Jessie S. Zamora <jzamss@gmail.com>
2022-03-11 12:01:56 +0800

    keyboard

---
+++ src/lib/ui/input.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let name;
+	export let value = null;
+	export let placeholder = '';
+	export let registerKeyListener;
+
+	let active = false;
+
+	const activate = () => {
+		active = true;
+		registerKeyListener({ name, active }, this);
+	};
+
+	const deactivate = () => {
+		active = false;
+		registerKeyListener({ name, active }, this);
+	};
+</script>
+
+<input
+	type="text"
+	bind:value
+	on:focus={activate}
+	on:blur={deactivate}
+	{placeholder}
+	class="text-lg px-2 py-1 border rounded shadow mt-2 focus:bg-yellow-100 caret-transparent"
+/>
--- src/routes/rptbill/index.svelte
+++ src/routes/rptbill/index.svelte
@@ -1,5 +1,33 @@
 <script>
 	import Header from '$lib/header.svelte';
+	import Keyboard from '$lib/keyboard/keyboard.svelte';
+	import Input from '$lib/ui/input.svelte';
+
+	let registerKeyListener = null;
+	let handleKey = null;
+	let entity = { name: '', address: '' };
 </script>
 
 <Header module="Real Property Tax Billing" />
+
+<h1 class="text-center p-4">Filipizen Kiosk</h1>
+<div class="flex flex-col items-center">
+	<a href="/queue" alt="Queueing System">Queue</a>
+	<a href="/rptbill" alt="Real Property Tax Billing">Real Property Tax Billing</a>
+	<a href="/ctc" alt="Community Tax Certification">Community Tax Certificate</a>
+</div>
+
+<Input bind:value={entity.name} name="name" {registerKeyListener} placeholder="Enter name" />
+<Input
+	bind:value={entity.address}
+	name="address"
+	{registerKeyListener}
+	placeholder="Enter Address"
+/>
+
+<Keyboard
+	bind:registerKeyListener
+	bind:handleKey
+	on:keydown={(event) => (entity = handleKey(entity, event))}
+	on:return={() => console.log('return pressed')}
+/>
