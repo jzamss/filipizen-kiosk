4511765c4540713f504e949c95446a03c8754f69
Jessie S. Zamora <jzamss@gmail.com>
2022-03-11 15:44:21 +0800

    rptbill update

--- src/routes/rptbill/index.svelte
+++ src/routes/rptbill/index.svelte
@@ -10,13 +10,6 @@
 
 <Header module="Real Property Tax Billing" />
 
-<h1 class="text-center p-4">Filipizen Kiosk</h1>
-<div class="flex flex-col items-center">
-	<a href="/queue" alt="Queueing System">Queue</a>
-	<a href="/rptbill" alt="Real Property Tax Billing">Real Property Tax Billing</a>
-	<a href="/ctc" alt="Community Tax Certification">Community Tax Certificate</a>
-</div>
-
 <Input bind:value={entity.name} name="name" {registerKeyListener} placeholder="Enter name" />
 <Input
 	bind:value={entity.address}
