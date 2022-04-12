571acefe195a693811d5b3aed0d81f01d4056a71
unknown <jdcanonoy21@gmail.com>
2022-04-12 09:46:19 +0800

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,7 +18,7 @@
 		<div class="grid grid-cols-3 grid-rows-3 m-20">
 			{#each menus as menu}
 			<a href="{menu.link}" alt="{menu.name}">
-				<div class="hover:bg-blue-800 h-60 text-center w-11/12">
+				<div class="h-60 text-center w-11/12">
 					<img src="/static/icons/{menu.icon}.svg" alt="{menu.name}" class="w-1/2 m-auto" />
 					<p class="pt-4 text-base font-bold">{menu.name}</p>
 				</div>
