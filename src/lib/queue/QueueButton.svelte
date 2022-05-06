bf85b25295f0a29d4a5beb9f5c0ba5fbcf646e35
unknown <jdcanonoy21@gmail.com>
2022-05-06 17:26:12 +0800

    update

--- src/lib/queue/QueueButton.svelte
+++ src/lib/queue/QueueButton.svelte
@@ -2,14 +2,12 @@
 	export let href = null;
 	export let title;
 	export let icon;
+
+	import Button from '$lib/ui/button.svelte';
 </script>
 
 {#if href}
-	<a {href} alt={title} class="m-auto">
-		<div>	
-			<p class="text-2xl font-bold text-center pt-5">{title}</p>
-		</div>
-	</a>
+	<Button {href} alt={title} caption="{title}" rightIcon="/static/icons/{title}.svg"/>
 {:else}
 	<button class="m-auto" on:click>
 		<div>
--- src/lib/ui/error.svelte
+++ src/lib/ui/error.svelte
@@ -4,7 +4,6 @@
 
 {#if error}
 	<div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative text-center" role="alert">
-		<strong class="font-bold">Failed!</strong>
 		<span class="block sm:inline">{error}</span>
   	</div>
 {/if}
