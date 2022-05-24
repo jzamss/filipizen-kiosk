54addb476b1ae933df02018aea6e81d69e51e83e
unknown <jdcanonoy21@gmail.com>
2022-05-24 13:16:06 +0800

    update

--- src/lib/footer-master.svelte
+++ src/lib/footer-master.svelte
@@ -4,15 +4,16 @@
 </script>
 
 
-<footer class="p-5 flex absolute bottom-0 w-full bg-gray-300">
-    <div class="w-2/6">
+<footer class="p-5 flex absolute bottom-0 w-full bg-white">
+    <div class="w-2/5">
+        <a href="/" alt="home icon link"><img src="/static/icons/home.svg" alt="home icon" class="w-20" /></a>
+    </div>
+    <div class="w-2/5">
         <a href="/" alt="LGU Logo">
-            <img src="/static/rameses.png" alt="Rameses Systems Inc Logo" class="w-60 pt-3"/>
+            <img src="/static/icons/rameses.svg" alt="Rameses Systems Inc Logo" class="m-auto w-40 bottom-0 absolute "/>
         </a>
     </div>
-    <div class="w-2/6">
-        <a href="/" alt="home icon link"><img src="/static/icons/home.svg" alt="home icon" class="w-20 m-auto" /></a>
-    </div>
+    
     <div class="m-auto time">
         <p class="text-4xl pl-5">{time}</p>
         <p class="text-2xl">{dt}</p>
